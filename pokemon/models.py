0a8f772c7e93ea9a53a529a18e0fbd7684b18cef
mallollaa <mah41411@gmail.com>
2023-01-19 01:58:22 +0300

    not done yet

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,32 +1,30 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 from datetime import datetime
 
 # Create your models here.
 class Pokemon (models.Model):
-    name =  models.CharField(max_length=30 ,default= "" ,blank=True)
+    name =  models.CharField(max_length=30)
     type = models.CharField()
-    hp = models.PositiveIntegerField
+
+  
+    class PokemonType(models.TextChoices):
+        WATER = 'WA'
+        GRASS = 'GR'
+        GHOST = 'GH'
+        STEEL = 'ST'
+        FAIRY = 'FA'
+
+    Pokemon_Type= models.CharField(  
+        max_length=100,
+        choices= PokemonType.choices,
+        
+    )
+
+    hp = models.PositiveIntegerField(default=0)
     active = models.BooleanField(default=True)
     name_fr = models.CharField(max_length=30 , default="" ,blank=True)
     name_ar = models.CharField(max_length=30 , default="" ,blank=True)
     name_jp = models.CharField(max_length=30 , default="",blank=True)
     created_at = models.DateTimeField(auto_now_add =True)
-    modified_at = models.DateTimeField(auto_now_add=True)
-class PokemonType(models.TextChoices):
-        WATER = 'WA',
-        GRASS = 'GR'
-        GHOST = 'GH'
-        STEEL = 'ST'
-        FAIRY = 'FA'
-Pokemon_Type= models.TextField(  
-    max_length=30,
-    choices= PokemonType.choices,
-    default= PokemonType.WATER,
-)
-    
-def is_upperclass(self):
-    return self.PokemonType in {
-        self.PokemonType.GHOST,
-        self.PokemonType.STEEL,
-}
+    modified_at = models.DateTimeField(auto_now=True)
+
