68922ba633c9b81a9a361d62d0d0e7c4fa7e4298
mallollaa <mah41411@gmail.com>
2023-01-19 01:39:53 +0300

    errors need to be fixed

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from datetime import datetime
 
 # Create your models here.
+class Pokemon (models.Model):
+    name =  models.CharField(max_length=30 ,default= "" ,blank=True)
+    type = models.CharField()
+    hp = models.PositiveIntegerField
+    active = models.BooleanField(default=True)
+    name_fr = models.CharField(max_length=30 , default="" ,blank=True)
+    name_ar = models.CharField(max_length=30 , default="" ,blank=True)
+    name_jp = models.CharField(max_length=30 , default="",blank=True)
+    created_at = models.DateTimeField(auto_now_add =True)
+    modified_at = models.DateTimeField(auto_now_add=True)
+class PokemonType(models.TextChoices):
+        WATER = 'WA',
+        GRASS = 'GR'
+        GHOST = 'GH'
+        STEEL = 'ST'
+        FAIRY = 'FA'
+Pokemon_Type= models.TextField(  
+    max_length=30,
+    choices= PokemonType.choices,
+    default= PokemonType.WATER,
+)
+    
+def is_upperclass(self):
+    return self.PokemonType in {
+        self.PokemonType.GHOST,
+        self.PokemonType.STEEL,
+}
