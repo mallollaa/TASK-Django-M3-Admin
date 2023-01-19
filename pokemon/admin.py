243d2c1fa43faba5bfd0741126a0c4076484fe67
mallollaa <mah41411@gmail.com>
2023-01-19 17:15:06 +0300

    fixing some issue

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Pokemon
-admin.site.register(Pokemon)
+# admin.site.register(Pokemon) this line without costimasitions
+@admin.register(Pokemon) # if we want to custimoise the admin page
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'hp' ,'active']
+    list_display_links= ['id', 'name']
+    # this changes happen in the admin page 
+    list_filter =['active'] # this func help us to now which one is active 
+
+    fieldsets = [ #we use this to coustimaze each items 
+        ("localizatons", 
+
+        {   "fields":['name_ar','name_fr','name_jp' ],
+        
+        }
+        )
+    ]
--- pokemon/models.py
+++ pokemon/models.py
@@ -4,15 +4,15 @@ from datetime import datetime
 # Create your models here.
 class Pokemon (models.Model):
     name =  models.CharField(max_length=30)
-    type = models.CharField()
+    type = models.CharField(max_length=2)
 
   
     class PokemonType(models.TextChoices):
-        WATER = 'WA'
-        GRASS = 'GR'
-        GHOST = 'GH'
-        STEEL = 'ST'
-        FAIRY = 'FA'
+        WATER = 'WA' , 'Water' # this is a hyper text it will show to the user 
+        GRASS = 'GR' , 'grass'
+        GHOST = 'GH' , 'ghoste'
+        STEEL = 'ST' , 'steel'
+        FAIRY = 'FA' , 'fairy'
 
     Pokemon_Type= models.CharField(  
         max_length=100,
@@ -22,7 +22,7 @@ class Pokemon (models.Model):
 
     hp = models.PositiveIntegerField(default=0)
     active = models.BooleanField(default=True)
-    name_fr = models.CharField(max_length=30 , default="" ,blank=True)
+    name_fr = models.CharField(max_length=30 , default="" ,blank=True) # blank mean we accept anything from the user
     name_ar = models.CharField(max_length=30 , default="" ,blank=True)
     name_jp = models.CharField(max_length=30 , default="",blank=True)
     created_at = models.DateTimeField(auto_now_add =True)
